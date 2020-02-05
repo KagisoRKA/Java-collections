e7541ffa3ea7c3550226686aeaea195a898c6a6a
Kagiso Raseroka <kagiso.raseroka@umuzi.org>
2020-02-05 16:43:00 +0200

    Updated

--- src/main/java/Collections.java
+++ src/main/java/Collections.java
@@ -4,25 +4,26 @@ public class Collections {
         LinkedList<Integer> queue = new LinkedList<>();
         queue.add(3);
         queue.add(2);
-        queue.add(2);
-        queue.add(2);
-        queue.add(2);
-        queue.add(2);
+        queue.add(7);
+        queue.add(5);
+        queue.add(7);
+        queue.add(7);
+        queue.add(8);
+        queue.add(8);
         int highUniqueNums =0;
-        LinkedList<Integer> checkedValues = new LinkedList<>();
-        for (int a=0; a<queue.size();a++){
-            int uniqueNum = 0;
-            for(int b=1; b<=subArraySize-1;b++){
-                if(!queue.get(a).equals(queue.get(a+b))){
-                    uniqueNum+=1;
-                }
+        String checkedNumbers = "";
+        for (int a = 0; a<queue.size(); a++) {
+            LinkedList<Integer> subArray = new LinkedList<>();
+            if (a+subArraySize-1 == queue.size()) { break; }
+            for (int b = 0; b < subArraySize; b++){
+                subArray.add(queue.get(a + b));
             }
-            if(uniqueNum > highUniqueNums){
-                highUniqueNums=uniqueNum;
-            }
-            if(queue.get(a).equals(queue.get(queue.size()-subArraySize))){
-                break;
-            }
+//            for (int value: subArray){
+//                if(){
+//
+//                }
+//            }
+            System.out.println(subArray);
         }
         return highUniqueNums;
     }
